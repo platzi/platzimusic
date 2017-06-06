6ff7fa72f5c04cb4ff88c538f6f6746872fce79f
Alfredo Barron <alfredobarronc@gmail.com>
2017-06-06 17:05:21 -0500

    Add bind to src
    Can see logo Vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='https://platzi.github.io/platzimusic/dist/logo.png')
+    img(:src='https://platzi.github.io/platzimusic/dist/logo.png')
     h1 PlatziMusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
